96fc6a840e2ad998048575cc5d4028cb35b88594
ChavinduShirantha <chavindushirantha@gmail.com>
2024-05-19 13:34:19 +0530

    Created Main Content Area

--- client/src/view/common/DefaultLayout/DefaultLayout.tsx
+++ client/src/view/common/DefaultLayout/DefaultLayout.tsx
@@ -1,12 +1,14 @@
 import {Component} from "react";
 import {Navbar} from "../Navbar/Navbar";
 import {Footer} from "../Footer/Footer";
+import {MainContent} from "../MainContent/MainContent";
 
 export class DefaultLayout extends Component {
     render() {
         return (
             <>
                 <Navbar/>
+                <MainContent/>
                 <Footer/>
             </>
         );
---
+++ client/src/view/common/MainContent/MainContent.tsx
@@ -0,0 +1,11 @@
+import {Component} from "react";
+
+export class MainContent extends Component {
+    render() {
+        return (
+            <>
+            <h1>Main Content</h1>
+            </>
+        );
+    }
+}
