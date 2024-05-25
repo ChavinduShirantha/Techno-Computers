a1f75edecf9bc85f3bc2d7759b224c0f57afa4b7
ChavinduShirantha <chavindushirantha@gmail.com>
2024-05-25 16:51:23 +0530

    Started to Create Admin Footer

--- client/src/view/pages/adminPages/AdminDefaultLayout/AdminDefaultLayout.tsx
+++ client/src/view/pages/adminPages/AdminDefaultLayout/AdminDefaultLayout.tsx
@@ -1,11 +1,13 @@
 import {Component} from "react";
 import {AdminNavBar} from "../AdminNavBar/AdminNavBar";
+import {AdminFooter} from "../AdminFooter/AdminFooter";
 
 export class AdminDefaultLayout extends Component {
     render() {
         return (
             <>
                 <AdminNavBar/>
+                <AdminFooter/>
             </>
         );
     }
