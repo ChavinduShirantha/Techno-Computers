6132f9c83888c444a578bacb1f2d6514a5399773
ChavinduShirantha <chavindushirantha@gmail.com>
2024-05-31 13:09:26 +0530

    Created Customer MainContent

--- client/src/view/pages/customerPages/CustomerDefaultLayout/CustomerDefaultLayout.tsx
+++ client/src/view/pages/customerPages/CustomerDefaultLayout/CustomerDefaultLayout.tsx
@@ -1,12 +1,14 @@
 import {Component} from "react";
 import {CustomerNavBar} from "../CustomerNavBar/CustomerNavBar";
 import {CustomerFooter} from "../CustomerFooter/CustomerFooter";
+import {CustomerMainContent} from "../CustomerMainContent/CustomerMainContent";
 
 export class CustomerDefaultLayout extends Component {
     render() {
         return (
             <>
                 <CustomerNavBar/>
+                <CustomerMainContent/>
                 <CustomerFooter/>
             </>
         );
---
+++ client/src/view/pages/customerPages/CustomerMainContent/CustomerMainContent.tsx
@@ -0,0 +1,9 @@
+import {Component} from "react";
+
+export class CustomerMainContent extends Component {
+    render() {
+        return (
+            <>Main Content</>
+        );
+    }
+}
