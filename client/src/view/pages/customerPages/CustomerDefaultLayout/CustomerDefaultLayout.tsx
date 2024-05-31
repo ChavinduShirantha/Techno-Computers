f0110a1e4e952b56dec67afe60cb6b604051d752
ChavinduShirantha <chavindushirantha@gmail.com>
2024-05-31 13:07:26 +0530

    Created Customer Footer

--- client/src/view/pages/customerPages/CustomerDefaultLayout/CustomerDefaultLayout.tsx
+++ client/src/view/pages/customerPages/CustomerDefaultLayout/CustomerDefaultLayout.tsx
@@ -1,11 +1,13 @@
 import {Component} from "react";
 import {CustomerNavBar} from "../CustomerNavBar/CustomerNavBar";
+import {CustomerFooter} from "../CustomerFooter/CustomerFooter";
 
 export class CustomerDefaultLayout extends Component {
     render() {
         return (
             <>
                 <CustomerNavBar/>
+                <CustomerFooter/>
             </>
         );
     }
