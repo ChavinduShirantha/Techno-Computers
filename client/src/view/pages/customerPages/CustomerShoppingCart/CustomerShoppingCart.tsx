7d3d6857fd525b902f30d8b282380295b336fd6d
ChavinduShirantha <chavindushirantha@gmail.com>
2024-05-31 15:51:21 +0530

    Updated Customer ShoppingCart

--- client/src/view/pages/customerPages/CustomerShoppingCart/CustomerShoppingCart.tsx
+++ client/src/view/pages/customerPages/CustomerShoppingCart/CustomerShoppingCart.tsx
@@ -1,5 +1,4 @@
 import {Component} from "react";
-import {Link} from "react-router-dom";
 import {CartItem} from "../../../../model/CartItem";
 
 interface CustomerShoppingCartProps {
@@ -9,18 +8,32 @@ interface CustomerShoppingCartProps {
 export class CustomerShoppingCart extends Component<CustomerShoppingCartProps> {
     render() {
         let total = 0;
+        let totCount = 0;
 
         return (
             <div className="flex justify-center items-start pt-10 pb-10 bg-[url(images/main_bg.jpg)] h-auto">
                 <div className="bg-[#232323] w-full mx-16 justify-center pr-10 pl-10 rounded-4">
-                    <div className="pb-2 pt-10 flex">
-                        <label className="text-[20px] pt-3 text-white font-bold px-2 pr-5 uppercase">Order ID :</label>
-                        <input type="text"
-                               className="block w-1/6 px-4 py-2 mt-2 bg-[#444544] text-white border
+                    <div className="flex flex-wrap w-full ">
+                        <div className="pb-2 pt-10 flex w-1/2">
+                            <label className="text-[20px] pt-3 text-white font-bold px-2 pr-5 uppercase">Order ID
+                                :</label>
+                            <input type="text"
+                                   className="block w-1/3 px-4 py-2 mt-2 bg-[#444544] text-white border
                                        rounded-md focus:border-[#2cc1fc] focus:ring-[#2cc1fc] focus:outline-none
                                        focus:ring focus:ring-opacity-40" placeholder="ORD-0001"
-                               name="oid"
-                        />
+                                   name="oid" id="oid"
+                            />
+                        </div>
+                        <div className="pb-2 pt-10 flex w-1/2">
+                            <label className="text-[20px] pt-3 text-white font-bold px-2 pr-5 uppercase">Order Date
+                                :</label>
+                            <input type="date"
+                                   className="block w-1/3 px-4 py-2 mt-2 bg-[#444544] text-white border
+                                       rounded-md focus:border-[#2cc1fc] focus:ring-[#2cc1fc] focus:outline-none
+                                       focus:ring focus:ring-opacity-40"
+                                   name="oDate" id="oDate"
+                            />
+                        </div>
                     </div>
 
                     <table className="w-full border mt-10 border-gray-500">
@@ -30,9 +43,12 @@ export class CustomerShoppingCart extends Component<CustomerShoppingCartProps> {
                             <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Image</th>
                             <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Product Name
                             </th>
-                            <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Unit Price</th>
+                            <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Unit Price
+                            </th>
                             <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Qty</th>
-                            <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Total Price</th>
+                            <th className="text-[14px] font-bold px-1 uppercase border border-gray-500">Total
+                                Price
+                            </th>
                         </tr>
                         </thead>
                         <tbody>
@@ -40,7 +56,8 @@ export class CustomerShoppingCart extends Component<CustomerShoppingCartProps> {
                             this.props.itemsList.length === 0 ?
                                 <tr>
                                     <td colSpan={6} className="h-36 px-1">
-                                        <p className="text-center text-[30px] text-white font-bold">You Have No Items In
+                                        <p className="text-center text-[30px] text-white font-bold">You Have No
+                                            Items In
                                             Your Shopping
                                             Cart.</p>
                                     </td>
@@ -48,9 +65,10 @@ export class CustomerShoppingCart extends Component<CustomerShoppingCartProps> {
                                 : this.props.itemsList.map((item) =>
                                     <tr className="text-white text-center px-1 h-20 border border-gray-500">
                                         <td className="px-1 border border-gray-500">{item.product.id}</td>
-                                        <td className="p-5 border border-gray-500"><img className="h-24 mx-auto rounded-lg"
-                                                                                        src={item.product.image}
-                                                                                        alt=""/>
+                                        <td className="p-5 border border-gray-500"><img
+                                            className="h-24 mx-auto rounded-lg"
+                                            src={item.product.image}
+                                            alt=""/>
                                         </td>
                                         <td className="px-1 border border-gray-500">{item.product.description}</td>
                                         <td className="px-1 border border-gray-500">{item.product.price + item.product.currency}</td>
@@ -58,16 +76,22 @@ export class CustomerShoppingCart extends Component<CustomerShoppingCartProps> {
                                         <td className="px-1 border
                                         border-gray-500">{(item.product.price * item.itemCount) + item.product.currency}</td>
                                         <td className="px-1 border border-gray-500 hidden">{total += (item.product.price * item.itemCount)}</td>
+                                        <td className="px-1 border border-gray-500 hidden">{totCount += item.itemCount}</td>
                                     </tr>
                                 )
                         }
                         </tbody>
                     </table>
-                    <div>
-                        <h1 className="float-left text-[20px] mt-10 font-bold px-1 uppercase text-white">Total : <span
-                            className="text-[20px] mt-10 font-bold px-1 uppercase text-white">{total + ".00 LKR"}</span>
+                    <div className="w-full">
+                        <h1 className="float-left text-[20px] mt-10 font-bold px-1 uppercase text-white">Total
+                            : <span
+                                className="text-[20px] mt-10 font-bold px-1 uppercase text-white">{total + ".00 LKR"}</span>
                         </h1>
-                        <button className="float-right mt-10 mb-10 pl-6 pr-6 pt-2 pb-2 bg-[#2cc1fc] text-[16px]
+                        <h1 className="float-right text-[20px] mt-10 font-bold px-1 uppercase text-white">Total Item Count
+                            : <span
+                                className="text-[20px] mt-10 font-bold px-1 uppercase text-white">{totCount}</span>
+                        </h1>
+                        <button className="float-right mt-24 mb-10 pl-6 pr-6 pt-2 pb-2 bg-[#2cc1fc] text-[16px]
                     font-bold text-[#e6f0e6] rounded uppercase border-[2px] border-[#2cc1fc]
                     hover:bg-[#444544] hover:text-[#2cc1fc] hover:border-[2px]
                     hover:border-[#2cc1fc] hover:scale-110 "> Purchase
